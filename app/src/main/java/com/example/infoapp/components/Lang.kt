4b65d3d4ce02e5eab60a684083041c71fd457f38
unknown <jainadi1717@gmail.com>
2023-12-04 14:45:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/infoapp/components/Lang.kt
@@ -0,0 +1,150 @@
+package com.example.infoapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun Lang(
+
+) {
+//    Column {
+//        Text(text = "Languages",
+//            color = Color.Blue,
+//            textAlign = TextAlign.Left,
+//            modifier = Modifier.padding(start = 26.dp)
+//
+//        )
+        Spacer(modifier = Modifier.height(2.dp))
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 25.dp, top = 15.dp, bottom = 0.dp)
+        ) {
+            val programmingLanguages =
+                listOf("C and C++ n", "Python n", "Java n", "Kotlin n", "Flutter n")
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Blue)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Language: ", color = Color.White)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+            for (language in programmingLanguages) {
+                item {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(Color(0xFF0000FF).copy(0.8f))
+                            .padding(15.dp)
+                    ) {
+                        Text(text = language, color = Color.White)
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.width(20.dp))
+                }
+            }
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Cyan)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "C and C++", color = Color.White)
+                }
+            }
+
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Cyan)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Python", color = Color.White)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Cyan)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Java", color = Color.White)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Cyan)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Kotlin", color = Color.White)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+
+            item {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Cyan)
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Flutter", color = Color.White)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+        }
+//    }
+
+}
